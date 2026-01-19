679a0dbab44f71b7cdb9b81847357f7421876c24
andrealangianese <andre.langia.01@gmail.com>
2026-01-19 15:40:25 +0100

    feat: importo e stampo il contenuto in pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import FinalFooter from "./components/FinalFooter"
 import FirstFooter from "./components/FirstFooter"
 import MainContent from "./components/MainContent"
 import MainHeader from "./components/MainHeader"
+import ComicsList from "./components/MainComic"
 
 function App() {
   return (
     <>
       <div className="container">
         <MainHeader />
+        <ComicsList />
         <div className="blue-backgroung"><MainContent /></div>
         <div className="image-background"><FirstFooter /></div>
         <div className="social"><FinalFooter /></div>
