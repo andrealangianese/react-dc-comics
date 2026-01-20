63e5cfeda1f80467c410de29cc99f08e86a35d0e
andrealangianese <andre.langia.01@gmail.com>
2026-01-20 14:22:42 +0100

    card pronta da importare

--- src/components/product/ProductComicCard.jsx
+++ src/components/product/ProductComicCard.jsx
@@ -1,6 +1,18 @@
 const ProductComicCard = (props) => {
     // destrutturo l'oggetto
 
-    const {thumb , series} = props;
+    const { thumb, series } = props;
     
+    //uso il riferimento di ciò che avevo già creato e lo sostituisco sotto
+    return(
+    // <div className="card-img ">
+    //     <img src={comic.thumb} alt={comic.series} />
+    //     <h3>{comic.series}</h3>
+    // </div>
+    <div className="card-img ">
+        <img src={thumb} alt={series} />
+        <h3>{series}</h3>
+    </div>
+    )
 }
+export default ProductComicCard
